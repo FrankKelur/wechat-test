844f9bb96b6dc12d71dc45031500b3e2cde7a044
zhaipengchao <chaopengzhai@163.com>
2019-04-04 17:21:35 +0800

    fix: 添加仰正

--- app.js
+++ app.js
@@ -1,12 +1,22 @@
 const http = require('http');
+const sha1 = require('sha1');
 
-const hostname = '127.0.0.1';
+const hostname = '0.0.0.0';
 const port = 8888;
 
 const server = http.createServer((req, res) => {
   console.log('req', req.url);
+  let params = req.url.split('?')[1].split('&').reduce((res, item) => { let [key, val] = item.split('='); res[key] = val; return res; }, {});
+  let { echostr, nonce, signature, timestamp } = params;
+  let token = 'zhaipengchao';
+  let arr = [token, nonce, timestamp].sort();
+  let result = sha1(arr.join(''));
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
+  if (result == signature) {
+    res.end(echostr);
+    return;
+  }
   res.end('Hello World\n');
 });
 
