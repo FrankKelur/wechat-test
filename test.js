77a2f9e8e58621895d509ed997ac0716ddc54021
zhaipengchao <chaopengzhai@163.com>
2019-04-09 16:10:03 +0800

    fix:

--- test.js
+++ test.js
@@ -4,10 +4,10 @@ function (error, response, body) {
   console.log('error:', error); // Print the error if one occurred
   console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
   console.log('body:', body); // Print the HTML for the Google homepage.
-  let ACCESS_TOKEN = body.access_token;
+  let ACCESS_TOKEN = JSON.parse(body).access_token;
   let url_get_ticket = `https://api.weixin.qq.com/cgi-bin/ticket/getticket?access_token=${ACCESS_TOKEN}&type=jsapi`
   request(url_get_ticket, function (error, response, body) {
-    let ticket = body.ticket;
+    let ticket = JSON.parse(body).ticket;
     console.log('api_ticket', ticket);
   });
 });
