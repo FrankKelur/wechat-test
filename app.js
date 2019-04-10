02a3eadea92afec4debbbcdcf1e1dc5108371c50
zhaipengchao <chaopengzhai@163.com>
2019-04-10 12:08:16 +0800

    fix

--- app.js
+++ app.js
@@ -17,13 +17,13 @@ var allowCrossDomain = function (req, res, next) {
 };
 app.use(allowCrossDomain);
 
+let appid = 'wx79e4dff84f30084b';
+let secret = '28fbe39d77453b42dd63cc607c790339';
 function getParams(req) {
   return (req.url.split('?')[1] || '').split('&').reduce((res, item) => { let [key, val] = item.split('='); res[key] = val; return res; }, {});
 }
 app.get('/get-ticket', (req, res) => {
   console.log('req', req.url);
-  let appid = 'wx79e4dff84f30084b';
-  let secret = '28fbe39d77453b42dd63cc607c790339';
   request('https://api.weixin.qq.com/cgi-bin/token?grant_type=client_credential&appid=' + appid + '&secret=' + secret,
     function (error, response, body) {
       let ACCESS_TOKEN = JSON.parse(body).access_token;
@@ -33,20 +33,24 @@ app.get('/get-ticket', (req, res) => {
         console.log('api_ticket', ticket);
         res.json({
           ticket,
-          ACCESS_TOKEN,
         });
       });
     });
 });
 app.get('/get-data', (req, res) => {
   console.log('req', req.url);
-  let { ACCESS_TOKEN, openid } = getParams(req);
-  let url = `https://api.weixin.qq.com/sns/userinfo?access_token=${ACCESS_TOKEN}&openid=${openid}&lang=zh_CN`;
-  request(url, function (error, response, body) {
-    res.json({
-      re: '200',
-      data: JSON.parse(body),
-    });
+  let { code } = getParams(req);
+  console.log(' openid: ', openid);
+  let access_token_url = `https://api.weixin.qq.com/sns/oauth2/access_token?appid=${appid}&secret=${secret}&code=${code}&grant_type=authorization_code`;
+  request(access_token_url, function (error, response, body) {
+    let { access_token, openid } = JSON.parse(body);
+    let url = `https://api.weixin.qq.com/sns/userinfo?access_token=${access_token}&openid=${openid}&lang=zh_CN`;
+    request(url, function (error, response, body) {
+      res.json({
+        re: '200',
+        data: JSON.parse(body),
+      });
+    })
   })
 });
 
