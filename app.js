fc91f17da0048a974803c4ec1dcf4d4aec73bd5f
zhaipengchao <chaopengzhai@163.com>
2019-04-13 01:30:14 +0800

    fix: auth

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const sha1 = require('sha1');
 const express = require('express')
 const app = express()
-app.use('/static', express.static('public'));
+// app.use('/static', express.static('public'));
 // app.use('/', express.static('public'));
 var request = require('request');
 const hostname = '0.0.0.0';
@@ -14,7 +14,7 @@ function getParams(req) {
   return (req.url.split('?')[1] || '').split('&').reduce((res, item) => { let [key, val] = item.split('='); res[key] = val; return res; }, {});
 }
 
-app.get('/', (req, res) => {
+app.get('/*', (req, res) => {
   console.log('req', req.url);
   let { echostr, nonce, signature, timestamp } = getParams(req);
   let token = 'zhaipengchao';
