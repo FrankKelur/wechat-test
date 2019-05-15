d9fbada2192dd4a611b0a5fa3d102db4f66ba799
zhaipengchao <chaopengzhai@163.com>
2019-05-15 14:23:58 +0800

    fix: 处理掉冲突

--- app.js
+++ app.js
@@ -1,79 +1,19 @@
 const sha1 = require('sha1');
 const express = require('express')
 const app = express()
-<<<<<<< HEAD
 app.use('/static', express.static('public'));
 //app.use('/', express.static('public'));
-=======
-// app.use('/static', express.static('public'));
-// app.use('/', express.static('public'));
->>>>>>> 0198847fa34da51dd867189d8fc4859cbfa9f6a5
 var request = require('request');
 const hostname = '0.0.0.0';
 const port = 8888;
 
-<<<<<<< HEAD
-var allowCrossDomain = function (req, res, next) {
-  // console.log('req.origin', req.headers)
-  res.header('Access-Control-Allow-Origin', req.headers.origin);
-  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
-  res.header('Access-Control-Allow-Headers', 'Content-Type');
-  res.header('Access-Control-Allow-Credentials', 'true');
-  next();
-};
-//app.use(allowCrossDomain);
-
 let appid = 'wx79e4dff84f30084b';
-let secret = '28fbe39d77453b42dd63cc607c790339';
-//let appid = 'wx661994a569e2f0e3'
-//let secret = '4ee9efca1c18ade6b163603dfb217522'
-function getParams(req) {
-  return (req.url.split('?')[1] || '').split('&').reduce((res, item) => { let [key, val] = item.split('='); res[key] = val; return res; }, {});
-}
-/*
-app.get('/get-ticket', (req, res) => {
-  console.log('req', req.url);
-  request('https://api.weixin.qq.com/cgi-bin/token?grant_type=client_credential&appid=' + appid + '&secret=' + secret,
-    function (error, response, body) {
-      let ACCESS_TOKEN = JSON.parse(body).access_token;
-      let url_get_ticket = `https://api.weixin.qq.com/cgi-bin/ticket/getticket?access_token=${ACCESS_TOKEN}&type=jsapi`
-      request(url_get_ticket, function (error, response, body) {
-        let ticket = JSON.parse(body).ticket;
-        console.log('api_ticket', ticket);
-        res.json({
-          ticket,
-        });
-      });
-    });
-});
-app.get('/get-data', (req, res) => {
-  console.log('req', req.url);
-  let { code } = getParams(req);
-  console.log(' openid: ', openid);
-  let access_token_url = `https://api.weixin.qq.com/sns/oauth2/access_token?appid=${appid}&secret=${secret}&code=${code}&grant_type=authorization_code`;
-  request(access_token_url, function (error, response, body) {
-    let { access_token, openid } = JSON.parse(body);
-    let url = `https://api.weixin.qq.com/sns/userinfo?access_token=${access_token}&openid=${openid}&lang=zh_CN`;
-    request(url, function (error, response, body) {
-      res.json({
-        re: '200',
-        data: JSON.parse(body),
-      });
-    })
-  })
-});
-*/
-app.get('/', (req, res) => {
-=======
-// let appid = 'wx79e4dff84f30084b';
-let appid = 'wx661994a569e2f0e3'
 
 function getParams(req) {
   return (req.url.split('?')[1] || '').split('&').reduce((res, item) => { let [key, val] = item.split('='); res[key] = val; return res; }, {});
 }
 
 app.get('/*', (req, res) => {
->>>>>>> 0198847fa34da51dd867189d8fc4859cbfa9f6a5
   console.log('req', req.url);
   let { echostr, nonce, signature, timestamp } = getParams(req);
   let token = 'zhaipengchao';
--- test.js
+++ test.js
@@ -1,15 +1,8 @@
 var request = require('request');
-<<<<<<< HEAD
-let appid ='wx661994a569e2f0e3'; 
-//let appid ='wx79e4dff84f30084b';
-let secret = 'e2ca297d0073ae01026906fb1359c83d'; //'4ee9efca1c18ade6b163603dfb217522';
-//let secret =  '28fbe39d77453b42dd63cc607c790339';
-=======
 let appid = 'wx661994a569e2f0e3';
 let secret = '4ee9efca1c18ade6b163603dfb217522';
 //wx661994a569e2f0e3
 // 4ee9efca1c18ade6b163603dfb217522
->>>>>>> 0198847fa34da51dd867189d8fc4859cbfa9f6a5
 request('https://api.weixin.qq.com/cgi-bin/token?grant_type=client_credential&appid='+appid+'&secret='+secret, 
 function (error, response, body) {
   console.log('error:', error); // Print the error if one occurred
